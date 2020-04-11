365b44ee59a8985d627a8eef4a9745676ee174e9
Shyam Kansagra <shyamkansagra@gmail.com>
2020-04-11 15:19:31 +0530

    Updates

---
+++ precache-manifest.cbae144d683af049f5153e156877bd35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d9a39c2b77fbeb5ff020d601f7c305e",
+    "url": "/trello-simulation/index.html"
+  },
+  {
+    "revision": "74939ddb2d19740b9376",
+    "url": "/trello-simulation/static/css/main.cf100d33.chunk.css"
+  },
+  {
+    "revision": "6db3b19e1136ec0afbd1",
+    "url": "/trello-simulation/static/js/2.bc52b9d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/trello-simulation/static/js/2.bc52b9d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74939ddb2d19740b9376",
+    "url": "/trello-simulation/static/js/main.6d7d493b.chunk.js"
+  },
+  {
+    "revision": "7d3675a536d6468c74d1",
+    "url": "/trello-simulation/static/js/runtime-main.9f055d1c.js"
+  }
+]);
